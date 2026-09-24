34032ef99019110470913c01270938f8c44478a2
kelvinka38 <kahoangkhanhthuan@gmail.com>
2026-09-24 11:20:14 +0700

    Add multiply function to calc.py with tests (#2)
    Co-authored-by: Metatron Core <core@metatron.local>

--- test_calc.py
+++ test_calc.py
@@ -1,11 +1,14 @@
 import unittest
 
-from calc import add, average
+from calc import add, average, multiply
 
 
 class CalcTest(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(3, 4), 12)
 
     def test_average(self):
         self.assertEqual(average([2, 4, 6]), 4)
